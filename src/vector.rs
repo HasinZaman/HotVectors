a9e01b35f7411d39a24307d49559844285533b63
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2024-11-15 10:45:23 -0500

    Updated vector.rs - Created traits for vectors  - VectorSpace  - Extremes - Split extremes macro for int and floats

--- src/vector.rs
+++ src/vector.rs
@@ -5,8 +5,54 @@ use std::{
 
 use serde::{Deserialize, Serialize};
 
+pub trait Extremes {
+    fn min() -> Self;
+    fn max() -> Self;
+    fn additive_identity() -> Self;
+    fn multiplicative_identity() -> Self;
+}
+
+pub trait VectorSpace<A> {
+    fn add(lhs: &Self, rhs: &Self) -> Self;
+
+    fn sub(lhs: &Self, rhs: &Self) -> Self;
+
+    fn dot(lhs: &Self, rhs: &Self) -> Self;
+
+    fn scalar_mult(lhs: &Self, scalar: &A) -> Self;
+}
+
 #[derive(PartialEq, Clone, Copy, Debug)]
 pub struct Vector<A: PartialEq + Clone + Copy, const CAP: usize>([A; CAP]);
+
+impl<
+        A: PartialEq + Clone + Copy + Add<Output = A> + Sub<Output = A> + Mul<Output = A>,
+        const CAP: usize,
+    > VectorSpace<A> for Vector<A, CAP>
+{
+    fn add(lhs: &Self, rhs: &Self) -> Self {
+        let mut iter = (lhs.0).iter().zip(rhs.0.iter()).map(|(a, b)| *a + *b);
+
+        Vector(array::from_fn(|_| iter.next().unwrap()))
+    }
+
+    fn sub(lhs: &Self, rhs: &Self) -> Self {
+        let mut iter = (lhs.0).iter().zip(rhs.0.iter()).map(|(a, b)| *a - *b);
+
+        Vector(array::from_fn(|_| iter.next().unwrap()))
+    }
+
+    fn dot(lhs: &Self, rhs: &Self) -> Self {
+        let mut iter = (lhs.0).iter().zip(rhs.0.iter()).map(|(a, b)| *a * *b);
+
+        Vector(array::from_fn(|_| iter.next().unwrap()))
+    }
+
+    fn scalar_mult(vec: &Self, scalar: &A) -> Self {
+        let mut iter = vec.0.iter().map(|a| *scalar * *a);
+        Vector(array::from_fn(|_| iter.next().unwrap()))
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct VectorSerial<A: Clone + Copy>(Vec<A>);
@@ -33,85 +79,69 @@ impl<A: PartialEq + Clone + Copy, const CAP: usize> Vector<A, CAP> {
         let mut iter = (0..CAP).map(|_| val);
         Vector(array::from_fn(|_| iter.next().unwrap()))
     }
-    // pub fn zero() -> Self {
-    //     todo!()
-    // }
-    // pub fn one() -> Self {
-    //     todo!()
-    // }
-
-    pub fn scalar_mul(scalar: A, vec: &Self) -> Self
-    where
-        A: Default + Mul<Output = A>,
-    {
-        let mut iter = vec.0.iter().map(|a| scalar * *a);
-        Vector(array::from_fn(|_| iter.next().unwrap()))
-    }
 }
 
-macro_rules! extremes {
+macro_rules! extremes_int {
     [$head: tt , $($tail: tt),+ ] => {
-        extremes!($head);
-        extremes![$($tail),+];
+        extremes_int!($head);
+        extremes_int![$($tail),+];
     };
     [$head: tt] => {
-        impl<const CAP: usize> Vector<$head, CAP> {
-            pub fn min() -> Self {
+        impl<const CAP: usize> Extremes for Vector<$head, CAP> {
+            fn min() -> Self {
                 let mut iter = (0..CAP)
                     .map(|_| $head::MIN);
                 Vector(array::from_fn(|_| iter.next().unwrap()))
             }
-            pub fn max() -> Self {
+            fn max() -> Self {
                 let mut iter = (0..CAP)
                     .map(|_| $head::MAX);
                 Vector(array::from_fn(|_| iter.next().unwrap()))
             }
-
+            fn additive_identity() -> Self {
+                let mut iter = (0..CAP)
+                    .map(|_| 0);
+                Vector(array::from_fn(|_| iter.next().unwrap()))
+            }
+            fn multiplicative_identity() -> Self {
+                let mut iter = (0..CAP)
+                    .map(|_| 1);
+                Vector(array::from_fn(|_| iter.next().unwrap()))
+            }
+        }
+    };
+}
+macro_rules! extremes_float {
+    [$head: tt , $($tail: tt),+ ] => {
+        extremes_float!($head);
+        extremes_float![$($tail),+];
+    };
+    [$head: tt] => {
+        impl<const CAP: usize> Extremes for Vector<$head, CAP> {
+            fn min() -> Self {
+                let mut iter = (0..CAP)
+                    .map(|_| $head::MIN);
+                Vector(array::from_fn(|_| iter.next().unwrap()))
+            }
+            fn max() -> Self {
+                let mut iter = (0..CAP)
+                    .map(|_| $head::MAX);
+                Vector(array::from_fn(|_| iter.next().unwrap()))
+            }
+            fn additive_identity() -> Self {
+                let mut iter = (0..CAP)
+                    .map(|_| 0.);
+                Vector(array::from_fn(|_| iter.next().unwrap()))
+            }
+            fn multiplicative_identity() -> Self {
+                let mut iter = (0..CAP)
+                    .map(|_| 1.);
+                Vector(array::from_fn(|_| iter.next().unwrap()))
+            }
         }
     };
 }
 
-extremes![i8, i16, i32, i64, i128];
-extremes![u8, u16, u32, u64, u128];
-extremes![f32, f64];
-
-impl<A, const CAP: usize> Add for Vector<A, CAP>
-where
-    A: Add<Output = A> + PartialEq + Clone + Copy,
-{
-    type Output = Vector<A, CAP>;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        let mut iter = (&self.0).iter().zip(rhs.0.iter()).map(|(a, b)| *a + *b);
-
-        Vector(array::from_fn(|_| iter.next().unwrap()))
-    }
-}
-
-impl<A, const CAP: usize> Sub for Vector<A, CAP>
-where
-    A: Sub<Output = A> + PartialEq + Clone + Copy,
-{
-    type Output = Vector<A, CAP>;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        let mut iter = (&self.0).iter().zip(rhs.0.iter()).map(|(a, b)| *a - *b);
-
-        Vector(array::from_fn(|_| iter.next().unwrap()))
-    }
-}
-
-impl<A, const CAP: usize> Mul for Vector<A, CAP>
-where
-    A: Default + Mul<Output = A> + Add<Output = A> + PartialEq + Clone + Copy,
-{
-    type Output = A;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        (&self.0)
-            .iter()
-            .zip(rhs.0.iter())
-            .map(|(a, b)| *a * *b)
-            .fold(A::default(), |acc, next| acc + next)
-    }
-}
+extremes_int![i8, i16, i32, i64, i128];
+extremes_int![u8, u16, u32, u64, u128];
+extremes_float![f32, f64];
