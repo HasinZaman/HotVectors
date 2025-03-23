74c93308d30eca601ca0e70378a4df8ccd1d21a0
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2025-03-23 15:09:26 -0400

    Refactored ids.rs - Due to the similarity between VectorId, PartitionId; id structs are now generated with a macro

--- src/db/component/ids.rs
+++ src/db/component/ids.rs
@@ -2,35 +2,29 @@ use std::ops::Deref;
 
 use uuid::Uuid;
 
-#[derive(Debug, Default, Clone, Copy, Hash, PartialEq, Eq, Ord)]
-pub struct VectorId(pub Uuid);
+macro_rules! ids {
+    [$head: tt , $($tail: tt),+ ] => {
+        ids!($head);
+        ids![$($tail),+];
+    };
+    [$head: tt] => {
+        #[derive(Debug, Default, Clone, Copy, Hash, PartialEq, Eq, Ord)]
+        pub struct $head(pub Uuid);
 
-impl Deref for VectorId {
-    type Target = Uuid;
+        impl Deref for $head {
+            type Target = Uuid;
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl PartialOrd for $head {
+            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                self.0.partial_cmp(&other.0)
+            }
+        }
+    };
 }
 
-impl PartialOrd for VectorId {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.0.partial_cmp(&other.0)
-    }
-}
-
-#[derive(Debug, Default, Clone, Copy, Hash, PartialEq, Eq, Ord)]
-pub struct PartitionId(pub Uuid);
-
-impl Deref for PartitionId {
-    type Target = Uuid;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl PartialOrd for PartitionId {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.0.partial_cmp(&other.0)
-    }
-}
+ids![ClusterId, VectorId, PartitionId];
