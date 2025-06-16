75a508c5c58ad2f747a6c8552d7fc0cc8c521611
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2025-06-16 00:42:08 -0400

    Add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use core::panic;
+use std::{fs::File, thread};
+
+use burn::backend::wgpu::WgpuDevice;
+use burn::backend::{wgpu::Wgpu, Autodiff};
+use burn::optim::AdamConfig;
+use burn::tensor::Tensor;
+use db::db_loop;
+use interface::{conn_pool, rest_api};
+use rand::Rng;
+use std::io;
+use tokio::{runtime, sync::mpsc::channel};
+use uuid::Uuid;
+use vector::Vector;
+
+use crate::db::component::{ids::PartitionId, partition::VectorEntry};
+
+pub mod db;
+pub mod interface;
+pub mod ops;
+pub mod vector;
+
+const VECTOR_CAP: usize = 128;
+
+// placeholder impl until better solution
+impl From<Vector<f32, VECTOR_CAP>> for Vector<f32, 2> {
+    fn from(value: Vector<f32, VECTOR_CAP>) -> Self {
+        // trait needs to be implemented but the conversion shouldn't be used in practice
+        panic!("This conversion is not implemented and should not be used in practice.");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,88 @@
+use core::panic;
 use std::{fs::File, thread};
 
-use db::db_loop;
-use interface::rest_api;
+use burn::backend::{wgpu::Wgpu, Autodiff};
+use HotVectors::db::db_loop;
+use HotVectors::interface::{conn_pool, rest_api};
 use std::io;
 use tokio::{runtime, sync::mpsc::channel};
-use vector::Vector;
+use HotVectors::vector::Vector;
 
-mod db;
-pub mod interface;
-pub mod ops;
-pub mod vector;
+use HotVectors::db::component::{ids::PartitionId, partition::VectorEntry};
+
+const VECTOR_CAP: usize = 128;
 
 fn main() {
+    // test umap trainer
+
+    // 6 random vectors
+    // const PARTITION_SIZE: usize = 150;
+    // let mut rng = rand::rng();
+    // let mut data: Vec<(PartitionId, Vector<f32, 3>)> = Vec::with_capacity(PARTITION_SIZE);
+
+    // for _ in 0..50 {
+    //     // Generate a random array of three f32 in [0.0, 1.0)
+    //     let arr: [f32; 3] = [rng.random(), rng.random(), rng.random()];
+    //     data.push((PartitionId(Uuid::new_v4()), Vector(arr)));
+    // }
+    // for _ in 0..50 {
+    //     // Generate a random array of three f32 in [0.0, 1.0)
+    //     let arr: [f32; 3] = [2.5 + rng.random::<f32>(), 2.5 + rng.random::<f32>(), 2.5 + rng.random::<f32>()];
+    //     data.push((PartitionId(Uuid::new_v4()), Vector(arr)));
+    // }
+    // for _ in 0..25 {
+    //     // Generate a random array of three f32 in [0.0, 1.0)
+    //     let arr: [f32; 3] = [2.5 + rng.random::<f32>(), 7.5 + rng.random::<f32>(), 2.5 + rng.random::<f32>()];
+    //     data.push((PartitionId(Uuid::new_v4()), Vector(arr)));
+    // }
+    // for _ in 0..25 {
+    //     // Generate a random array of three f32 in [0.0, 1.0)
+    //     let arr: [f32; 3] = [2.5 + rng.random::<f32>(), rng.random::<f32>(), rng.random::<f32>()];
+    //     data.push((PartitionId(Uuid::new_v4()), Vector(arr)));
+    // }
+
+    // let config: UMapTrainingConfig = UMapTrainingConfig {
+    //     optimizer: AdamConfig::new(),
+    //     attractive_size: 16,
+    //     repulsive_size: 8,
+    //     epoch: 3,
+    //     seed: 42,
+    //     learning_rate: 0.01,
+    // };
+
+    // type B = Wgpu<f32, i32>;
+    // type AutoB = Autodiff<B>;
+
+    // let model =
+    //     train_umap::<AutoB, Vector<f32, 3>, Vector<f32, 2>, 3, 3, 16, 2>(data.clone(), config);
+
+    // println!("Projected positions:");
+    // let device = WgpuDevice::default();
+    // for (partition_id, original_vec) in &data {
+    //     let projected: Vector<f32, 2> = model.forward(*original_vec);
+    //     println!("Partition {:?} ({:?}) -> Projected: {:?}", partition_id, original_vec, projected);
+    // }
+
+    // // Group 1 (low range)
+    // for _ in 0..25 {
+    //     let arr: [f32; 3] = [rng.random(), rng.random(), rng.random()];
+    //     let test_vec = Vector(arr);
+    //     let projected: Vector<f32, 2> = model.forward(test_vec);
+    //     println!("Test (Group 1) {:?} -> Projected: {:?}", test_vec, projected);
+    // }
+
+    // // Group 2 (high range)
+    // for _ in 0..30 {
+    //     let arr: [f32; 3] = [
+    //         2.5 + rng.random::<f32>(),
+    //         2.5 + rng.random::<f32>(),
+    //         2.5 + rng.random::<f32>(),
+    //     ];
+    //     let test_vec = Vector(arr);
+    //     let projected: Vector<f32, 2> = model.forward(test_vec);
+    //     println!("Test (Group 2) {:?} -> Projected: {:?}", test_vec, projected);
+    // }
+
     let file = File::create("debug.log");
     let file = match file {
         Ok(file) => file,
@@ -24,14 +95,22 @@ fn main() {
         .with_writer(file)
         .init();
     const DB_THREADS: usize = 10;
-    const INTERFACE_THREADS: usize = 2;
+    const INTERFACE_THREADS: usize = 4;
 
     let (cmd_sender, cmd_receiver) = channel(64);
 
     let db_thread = thread::Builder::new()
         .name("db_thread".to_string())
         .spawn(move || {
-            db_loop::<f32, Vector<f32, 2>, 20, 2, 10, DB_THREADS>(cmd_receiver);
+            db_loop::<
+                Autodiff<Wgpu<f32, i32>>,
+                f32,
+                Vector<f32, VECTOR_CAP>,
+                20,
+                VECTOR_CAP,
+                128,
+                DB_THREADS,
+            >(cmd_receiver);
         });
 
     let rt = runtime::Builder::new_multi_thread()
@@ -41,6 +120,6 @@ fn main() {
         .unwrap();
 
     rt.block_on(async {
-        rest_api::input_loop::<f32, Vector<f32, 2>>(cmd_sender).await;
+        conn_pool::input_loop::<f32, Vector<f32, VECTOR_CAP>>(cmd_sender, None).await;
     });
 }
